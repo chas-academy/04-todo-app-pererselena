542532aa1b68d7ef5a7d76d2d5445952c8cb4463
Elena Perers <elena.perers@chasacademy.se>
2019-01-20 14:24:32 +0100

    Ändrat namn i footer

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -7,7 +7,7 @@
 
 <footer class="site-footer">
     <div class="small-container">
-        <p class="text-center">Made by <a href="#">Your Name Here</a></p>
+        <p class="text-center">Made by <a href="#">Elena Perers</a></p>
     </div>
 </footer>
 
